bc57706c7c42e7255526a2dc12e15ed5d40aeb1c
Fabricio <fabemanrique@hotmail.com>
2021-08-30 16:48:13 -0300

    Basic Empty Project Structure

---
+++ ObsidianGameTools/ObsidianGameTools.cpp
@@ -0,0 +1,10 @@
+﻿// ObsidianGameTools.cpp : Defines the entry point for the application.
+//
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello CMake." << std::endl;
+	return EXIT_SUCCESS;
+}
