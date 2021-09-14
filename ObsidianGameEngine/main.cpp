41084cca5eca81c789464317c169fec43ec81bd6
Fabricio Manrique <fabricio.manrique@jamcity.com>
2021-09-14 17:23:44 -0300

    Added minimum SDL dependency
    Added CMake Fetch Content command for downloading and linking SDL as a dependency for the ObsidianGameEngine project
    Added a minimum Window using SDL in ObsidianGameEngine project

--- ObsidianGameEngine/main.cpp
+++ ObsidianGameEngine/main.cpp
@@ -1,8 +1,49 @@
 // Obsidian Game Engine main entry point
 
+#include <SDL.h>
 #include "vulkan/vulkan.h"
 
-int main()
+#include <stdio.h>
+
+int main( int argc, char* args[] )
 {
+    SDL_Window* window = nullptr;
+    
+    //The surface contained by the window
+    SDL_Surface* screenSurface = nullptr;
+
+    //Initialize SDL
+    if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+    {
+        printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+    }
+    else
+    {
+        //Create window
+        window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_SHOWN );
+        if( window == nullptr )
+        {
+            printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+        }
+        else
+        {
+            //Get window surface
+            screenSurface = SDL_GetWindowSurface( window );
+
+            //Fill the surface white
+            SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
+            
+            //Update the surface
+            SDL_UpdateWindowSurface( window );
+
+            //Wait two seconds
+            SDL_Delay( 2000 );
+        }
+    }
+    //Destroy window
+    SDL_DestroyWindow( window );
+
+    //Quit SDL subsystems
+    SDL_Quit();
     return 0;
 }
