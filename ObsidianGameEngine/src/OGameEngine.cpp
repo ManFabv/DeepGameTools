2b3a7e007207bb7202d79a6a7b76bfc0b88d657b
Fabricio Manrique <fabricio.manrique@jamcity.com>
2021-09-20 16:43:55 -0300

    Added asserts to OGameEngine

--- ObsidianGameEngine/src/OGameEngine.cpp
+++ ObsidianGameEngine/src/OGameEngine.cpp
@@ -3,63 +3,58 @@
 #include "OGameEngine/OGameEngine.h"
 #include <SDL.h>
 #include "vulkan/vulkan.h"
+#include <cassert>
 
 #include "OWindow/OWindow.h"
 
-#include <stdio.h>
-
 int OGameEngine::Main::OGameEngine::StartGame ( int argc, char* args[] )
 {
+    SubModules::OWindow::OWindow owindow;
+
     SDL_Window* window = nullptr;
     
     //The surface contained by the window
     SDL_Surface* screenSurface = nullptr;
 
     //Initialize SDL
-    if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+    int sdl_init_result = SDL_Init( SDL_INIT_VIDEO );
+    
+    assert( sdl_init_result >= 0 );
+
+    //Create window
+    window = SDL_CreateWindow( "Obsidian Game Engine Tools", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_SHOWN );
+    
+    assert( window != nullptr );
+    
+    //Get window surface
+    screenSurface = SDL_GetWindowSurface( window );
+
+    //Main loop flag
+    bool quit = false;
+
+    //Event handler
+    SDL_Event e;
+
+    //While application is running
+    while( !quit )
     {
-        printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
-    }
-    else
-    {
-        //Create window
-        window = SDL_CreateWindow( "Obsidian Game Engine Tools", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_SHOWN );
-        if( window == nullptr )
+        //Handle events on queue
+        while( SDL_PollEvent( &e ) != 0 )
         {
-            printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
-        }
-        else
-        {
-            //Get window surface
-            screenSurface = SDL_GetWindowSurface( window );
-
-            //Main loop flag
-            bool quit = false;
-
-            //Event handler
-            SDL_Event e;
-
-            //While application is running
-            while( !quit )
+            //User requests quit
+            if( e.type == SDL_QUIT )
             {
-                //Handle events on queue
-                while( SDL_PollEvent( &e ) != 0 )
-                {
-                    //User requests quit
-                    if( e.type == SDL_QUIT )
-                    {
-                        quit = true;
-                    }
-                }
-
-                //Fill the surface white
-                SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
-                
-                //Update the surface
-                SDL_UpdateWindowSurface( window );
+                quit = true;
             }
         }
+
+        //Fill the surface white
+        SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0x00, 0x00, 0x00 ) );
+        
+        //Update the surface
+        SDL_UpdateWindowSurface( window );
     }
+
     //Destroy window
     SDL_DestroyWindow( window );
 
